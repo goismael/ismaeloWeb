4e3081ad9f3693081290c0d5cb9f63f8bb80aae3
Ismael Ouedraogo <ismael44131@outlook.com>
2019-04-16 10:20:16 -0400

    My website: ismaelo.com

---
+++ ClassLibrary1/Models/BaseField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsmaelOuedraogo.DB
+{
+    public class Class1
+    {
+
+
+    }
+}
